b8f2df0634269e2cb5a0b54a5c2092517f8d894b
Fanos003 <fanos1990@hotmail.com>
2025-03-27 23:41:08 +0200

    Add files via upload

---
+++ craterfi-sitev8/src/CraterFiLanding.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CraterFiLanding = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🚀 Welcome to CraterFi</h1>
+      <p>Own your piece of space. Meme-fueled. Moon-bound.</p>
+    </div>
+  )
+}
+
+export default CraterFiLanding
